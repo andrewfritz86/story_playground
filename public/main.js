16608af70ec85a4992d63501fa5df2326abe8efb
andrewfritz86 <andrew.fritz86@gmail.com>
2015-01-27 11:55:45 -0500

    setup

--- public/main.js
+++ public/main.js
@@ -1,1 +1,22 @@
 console.log("mainjslinked")
+//grab DOM elements
+var $storyBox = $("#story-box");
+var $userEntry = $("#entry-form");
+var $entryButton = $("#enter-text");
+
+
+//add eventlistener to entryButton
+
+$entryButton.on("click",function(){
+	addEntryModel();
+})
+
+function addEntryModel(){
+	console.log('addEntryModel fired');
+	var content = $userEntry.val();
+	addEntryView(content);
+}
+
+function addEntryView(content){
+	console.log(content);
+}
